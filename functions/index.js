2c228fbc2c28cfd889e113513898f86f1e5825c5
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-04-11 16:38:47 -0500

    middleware

--- functions/index.js
+++ functions/index.js
@@ -40,11 +40,45 @@ app.get('/projects', (request, response) => {
         .catch(err => console.error(err));
 })
 
-app.post('/project', (request, response) => {
+// Check auth token to see if user has logged in
+const FBAuth = (request, response, next) => {
+    let idToken;
+    if(request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+        idToken = request.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return response.status(403).json({ error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        request.user = decodedToken;
+        console.log(decodedToken);
+        return db.collection('users')
+            .where('userId', '==', request.user.uid)
+            .limit(1)
+            .get();
+    })
+    .then(data => {
+        request.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch(err => {
+        console.error('Error while verifying token', err);
+        return response.status(403).json(err);
+    })
+}
+
+//Post one project
+app.post('/project', FBAuth, (request, response) => {
+    if (request.body.body.trim() === '') {
+        return response.status(400).json({ body: 'Body must not be empty' });
+    }
+
     const newProject = {
         body: request.body.body,
-        userHandle: request.body.userHandle,
-        createdAt: newDate().toISOString()
+        userHandle: request.user.handle,
+        createdAt: new Date().toISOString()
     };
 
     db
